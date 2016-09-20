b29341cdda9e6e900b8379f4caddf10e954908fc
Rene Abel Jensen <nemesisabel1980@gmail.com>
2016-09-20 11:43:02 +0200

    Upload before styling

--- index.php
+++ index.php
@@ -19,17 +19,15 @@
  </header>
          <?php 
 		 $jokedata = $connection->query("SELECT * FROM jokes ORDER BY id DESC"); 
-		 $joke = $jokedata->fetch_assoc();
+		 while($joke = $jokedata->fetch_assoc()){
 		 ###############################################################################################
 		 # Oh my god - I need a way to render ALL records from the database, not only the last one :-( #
 		 # This makes me sick...                                                                       # 
 		 ###############################################################################################
-		 print_r($joke);
 			echo '<!-- single Chuck Norris joke start -->
 			<div class="joke">
-					<img src="' . $joke['img'] . '" class="norris_pic" alt="Chuck Norris caricature"/>
-					<h2>' . $joke['joke'] .  '</h2>	       
-            </div>';
-			echo '<!-- single joke end -->';
+					<img src="' . $joke['img'] . '" class="norris_pic" alt="Chuck Norris caricature"/> <h2>' . $joke['joke'] .  '</h2>	       
+           </div>';
+			echo '<!-- single joke end -->';}
 		 ?>  
 </body> </html>
